ca0264ffce2912d192bf04834522cee564516c31
BuildTools <unconfigured@null.spigotmc.org>
2021-12-20 22:18:31 +0100

    new file:   .cproject 	new file:   .project 	new file:   .settings/language.settings.xml 	new file:   src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : ContactTracer.cpp
+// Author      : Andrea Roccaccino, Federico Masssaro
+// Version     : Alpha 0.1
+// Description : Helps determining chance of infection in a node network
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
