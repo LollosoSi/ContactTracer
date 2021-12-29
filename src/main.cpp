a7a2da4c8a449104a380bfaf6394abf4d7380a44
BuildTools <unconfigured@null.spigotmc.org>
2021-12-29 13:23:29 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -51,36 +51,36 @@ int main() {
 	}
 	delete [] buf;
 
-	g.add_contact(string("1"), string("5"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("5"), string("9"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("10"), string("9"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("10"), string("11"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("6"), string("11"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("6"), string("12"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("7"), string("12"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("7"), string("4"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("3"), string("4"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("3"), string("2"), 0.1, 2, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("1"), string("5"), 0.1, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("5"), string("9"), 0.1, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("10"), string("9"), 1, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("10"), string("11"), 2, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("6"), string("11"), 0.1, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("6"), string("12"), 0.1, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("7"), string("12"), 0.01, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("7"), string("4"), 0.01, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("3"), string("4"), 0.0001, 0.5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("3"), string("2"), 1, 5, string("2021-02-02 23:59"), rule);
 
-	g.add_contact(string("12"), string("17"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("20"), string("17"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("20"), string("19"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("19"), string("16"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("13"), string("20"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("14"), string("20"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("15"), string("18"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("15"), string("9"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("16"), string("18"), 0.1, 2, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("12"), string("17"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("20"), string("17"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("20"), string("19"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("19"), string("16"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("13"), string("20"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("14"), string("20"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("15"), string("18"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("15"), string("9"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("16"), string("18"), 0.5, 5, string("2021-02-02 23:59"), rule);
 
 
-	g.add_contact(string("14"), string("8"), 0.1, 2, string("2021-02-02 23:59"), rule);
-	g.add_contact(string("8"), string("4"), 0.1, 2, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("14"), string("8"), 0.5, 5, string("2021-02-02 23:59"), rule);
+	g.add_contact(string("8"), string("4"), 0.5, 5, string("2021-02-02 23:59"), rule);
 
 
 	writeToFile();
 
 
-	g.mark_infected(string("9"), 1, rule);
+	g.mark_infected(string("1"), 1, rule);
 	//g.mark_infected(string("nodo4"), 1);
 
 	// We don't need to propagate rule since it is handled when marking and when setting contact
